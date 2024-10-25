39b9b344be654f2265b42c651174753b2e236113
rahulKadam1264 <rahul.kadam@somaiya.edu>
2024-10-26 04:34:41 +0530

    3d model splinetool

--- CulturAI/src/pages/Pricing.jsx
+++ CulturAI/src/pages/Pricing.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Spline from '@splinetool/react-spline'
 const Pricing = () => {
   return (
     <div className='bg-gradient-to-b from-gray-900 to-black text-white min-h-screen w-[99vw]  flex items-center justify-center p-4'>
-      hrbdfhr
+      <Spline scene='https://prod.spline.design/am-NqLRRHxIgQMk0/scene.splinecode'/>
     </div>
   )
 }
