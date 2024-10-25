fb9a49289d88dbb5547fd54dd4b8c80dd19dcdcd
Pradyum Mistry <39883432+altf4-games@users.noreply.github.com>
2024-10-26 03:58:30 +0530

    Added Firebase Auth

--- CulturAI/src/App.jsx
+++ CulturAI/src/App.jsx
@@ -9,6 +9,25 @@ import Sidebar from './components/Sidebar'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSqMOYZzRimSPc2ygGqX_QtIuHCY_ecmo",
+  authDomain: "culturai-9fcdc.firebaseapp.com",
+  projectId: "culturai-9fcdc",
+  storageBucket: "culturai-9fcdc.appspot.com",
+  messagingSenderId: "531151773387",
+  appId: "1:531151773387:web:9b1e9a0f83932c43f9454e",
+  measurementId: "G-H456CS3063"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
 function App() {
   return (
     <>
