4278db121edcdce75b08f384b16fa6e1d556fa9c
Pradyum Mistry <39883432+altf4-games@users.noreply.github.com>
2024-10-26 11:42:23 +0530

    Added Scrolling

--- CulturAI/src/App.jsx
+++ CulturAI/src/App.jsx
@@ -1,30 +1,29 @@
-import { useState } from 'react'
-import CampaignDashboard from './pages/CampaignDashboard'
+import React, { useRef } from 'react';
+import CampaignDashboard from './pages/CampaignDashboard';
 import Dashboardfinal from './pages/Dashboardfinal';
-import Navbar from './components/Navbar'
-import LandingPage from './pages/LandingPage'
-import ContactUs from './pages/ContactUs'
-import Footer from './components/Footer'
-import Sidebar from './components/Sidebar'
-import './App.css'
+import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
+import ContactUs from './pages/ContactUs';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Pricing from './pages/Pricing';
 import Features from './pages/Features';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import MarketingAnalysis from "./pages/MarketingAnalysis"
-
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import MarketingAnalysis from './pages/MarketingAnalysis';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSqMOYZzRimSPc2ygGqX_QtIuHCY_ecmo",
-  authDomain: "culturai-9fcdc.firebaseapp.com",
-  projectId: "culturai-9fcdc",
-  storageBucket: "culturai-9fcdc.appspot.com",
-  messagingSenderId: "531151773387",
-  appId: "1:531151773387:web:9b1e9a0f83932c43f9454e",
-  measurementId: "G-H456CS3063"
+  apiKey: 'AIzaSyDSqMOYZzRimSPc2ygGqX_QtIuHCY_ecmo',
+  authDomain: 'culturai-9fcdc.firebaseapp.com',
+  projectId: 'culturai-9fcdc',
+  storageBucket: 'culturai-9fcdc.appspot.com',
+  messagingSenderId: '531151773387',
+  appId: '1:531151773387:web:9b1e9a0f83932c43f9454e',
+  measurementId: 'G-H456CS3063',
 };
 
 // Initialize Firebase
@@ -35,32 +34,29 @@ export const auth = getAuth(app);
 function App() {
   return (
     <>
-    <Router>
-      <div className="flex">
-        <Sidebar /> 
-        <div className="flex-grow bg-gradient-to-b from-gray-900 to-gray-800 text-white min-h-screen">
-          <Navbar />
-          
-
+      <Router>
+        <div className="flex">
+          <Sidebar /> 
+          <div className="flex-grow bg-gradient-to-b from-gray-900 to-gray-800 text-white min-h-screen">
+            <Navbar />
+            
             <Routes>
               <Route path="/" element={<>
                   <LandingPage />
-                  <Features/>
-                  <Pricing/>
-                  <ContactUs />
-
+                  <div id="features"><Features /></div>
+                  <div id="pricing"><Pricing /></div>
+                  <div id="contact-us"><ContactUs /></div>
               </>} />
               <Route path="/dashboard" element={<Dashboardfinal />} />
               <Route path="/analytics" element={<MarketingAnalysis />} />
-
             </Routes>
             
             <Footer />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
--- CulturAI/src/components/Navbar.jsx
+++ CulturAI/src/components/Navbar.jsx
@@ -1,58 +1,53 @@
 // src/Navbar.js
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../App"; // Import Firebase authentication
+import { auth } from "../App"; // Firebase authentication
 import { signInWithPopup, GoogleAuthProvider, signInAnonymously } from "firebase/auth";
 import { useCurrentUser } from '../authService';
 import { FcGoogle } from "react-icons/fc";
 import { FaUserSecret } from "react-icons/fa6";
 
 function Navbar() {
-
   const currentUser = useCurrentUser();
-  
-
   const [isSpinning, setIsSpinning] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [user, setUser] = useState(null);
 
-  
-
-  // Spin logo animation
   const handleLogoClick = () => {
     setIsSpinning(true);
-    setTimeout(() => setIsSpinning(false), 1000); // Stop spin after 1 second
+    setTimeout(() => setIsSpinning(false), 1000);
   };
 
-  // Handle Google Login
-    const handleGoogleLogin = async () => {
+  const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, provider);
       setUser(result.user);
-      setIsLoginModalOpen(false); // Close the modal
+      setIsLoginModalOpen(false);
     } catch (error) {
       console.error("Error with Google sign-in:", error.message);
     }
   };
 
-  // Handle Anonymous Login
   const handleAnonymousLogin = async () => {
     try {
       const result = await signInAnonymously(auth);
       setUser(result.user);
-      setIsLoginModalOpen(false); // Close the modal
+      setIsLoginModalOpen(false);
     } catch (error) {
       console.error("Error with Anonymous sign-in:", error.message);
     }
+  };
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <nav className="bg-gradient-to-r from-gray-900 to-gray-800 text-white fixed w-full z-20 shadow-md transition-shadow duration-300 hover:shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
-          {/* Logo */}
           <div className="flex-shrink-0">
             <h1
               onClick={handleLogoClick}
@@ -65,41 +60,23 @@ function Navbar() {
           </div>
 
           {/* Menu Items */}
-          {
-            !currentUser && (
-              <div className="hidden md:flex space-x-8 font-medium">
-            {["Home", "Features",  "Pricing", "Contact Us"].map(
-              (item) => (
-                <Link
-                  key={item}
-                  to={
-                    item === "Home"
-                      ? "/"
-                      : `/${item.toLowerCase().replace(" ", "-")}`
-                  }
-                  className="relative group transition duration-300"
-                >
-                  <span className="hover:text-teal-400">{item}</span>
-                  {/* Underline Effect */}
-                  <span className="absolute left-0 bottom-0 w-full h-0.5 bg-teal-400 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </Link>
-              )
-            )}
+          <div className="hidden md:flex space-x-8 font-medium">
+            {["Home", "Features", "Pricing", "Contact Us"].map((item) => (
+              <button
+                key={item}
+                onClick={() => handleScroll(item.toLowerCase().replace(" ", "-"))}
+                className="relative group transition duration-300 text-white hover:text-teal-400 font-semibold"
+              >
+                {item}
+              </button>
+            ))}
           </div>
-            )
-          }
 
-          {/* Login/Signup or User Profile and Mobile Menu */}
           <div className="flex items-center space-x-4">
             {user ? (
               <div className="flex items-center space-x-2">
-                {/* Display user's profile picture if available */}
                 {user.photoURL ? (
-                  <img
-                    src={user.photoURL}
-                    alt="Profile"
-                    className="w-8 h-8 rounded-full"
-                  />
+                  <img src={user.photoURL} alt="Profile" className="w-8 h-8 rounded-full" />
                 ) : (
                   <span className="w-8 h-8 rounded-full bg-gray-500 flex items-center justify-center text-white">
                     {user.displayName ? user.displayName[0] : "A"}
@@ -117,50 +94,30 @@ function Navbar() {
                 Login / Sign Up
               </button>
             )}
-
-            {/* Mobile Menu Button */}
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="md:hidden focus:outline-none"
             >
-              <svg
-                className="w-6 h-6 text-gray-200"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
+              <svg className="w-6 h-6 text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
             </button>
           </div>
         </div>
       </div>
 
-      {/* Mobile Menu */}
       {isMenuOpen && (
         <div className="md:hidden bg-gray-800">
           <div className="px-4 py-2 space-y-2">
-            {["Home", "Features", "Services", "Pricing", "Contact Us"].map(
-              (item) => (
-                <Link
-                  key={item}
-                  to={
-                    item === "Home"
-                      ? "/"
-                      : `/${item.toLowerCase().replace(" ", "-")}`
-                  }
-                  className="block text-left py-2 hover:text-teal-400 transition duration-300 font-semibold"
-                >
-                  {item}
-                </Link>
-              )
-            )}
+            {["Home", "Features", "Pricing", "Contact Us"].map((item) => (
+              <button
+                key={item}
+                onClick={() => handleScroll(item.toLowerCase().replace(" ", "-"))}
+                className="block text-left py-2 hover:text-teal-400 transition duration-300 font-semibold text-white"
+              >
+                {item}
+              </button>
+            ))}
             {!user && (
               <button
                 onClick={() => setIsLoginModalOpen(true)}
@@ -173,24 +130,21 @@ function Navbar() {
         </div>
       )}
 
-      {/* Login Modal */}
       {isLoginModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm text-center">
-            <h2 className="text-2xl font-semibold mb-6 text-gray-800">
-              Welcome to CulturAI
-            </h2>
+            <h2 className="text-2xl font-semibold mb-6 text-gray-800">Welcome to CulturAI</h2>
             <button
               onClick={handleGoogleLogin}
               className="bg-blue-500 text-white flex items-center justify-center gap-4 px-4 py-2 rounded-full mb-4 w-full transition hover:bg-blue-600"
             >
-             <FcGoogle size={25}/> Sign in with Google
+              <FcGoogle size={25} /> Sign in with Google
             </button>
             <button
               onClick={handleAnonymousLogin}
               className="bg-gray-500 text-white flex items-center justify-center gap-4 px-4 py-2 rounded-full w-full transition hover:bg-gray-600"
             >
-             <FaUserSecret size={25} color="black"/> Sign in Anonymously
+              <FaUserSecret size={25} color="black" /> Sign in Anonymously
             </button>
             <button
               onClick={() => setIsLoginModalOpen(false)}
